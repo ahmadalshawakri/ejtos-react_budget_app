7933fbb428a3802fcc47539c0b217a624b7063a5
ahmadalshawakri <ashawakri@gmail.com>
2023-03-11 22:59:33 +0300

    Part C Task 1

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,29 @@
+import React, { useContext, useRef } from "react";
+import { AppContext } from "../context/AppContext";
+const Budget = () => {
+  const budgetRef = useRef("");
+  const { budget } = useContext(AppContext);
+  const { dispatch } = useContext(AppContext);
 
+  const changeBudget = (event) => {
+    const edidtedBudget = +event.target.value;
+    dispatch({
+      type: "SET_BUDGET",
+      payload: edidtedBudget,
+    });
+  };
+
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: £</span>
+      <input
+        type="number"
+        step={10}
+        max={20000}
+        onChange={changeBudget}
+        value={budget}
+      />
+    </div>
+  );
+};
+export default Budget;
